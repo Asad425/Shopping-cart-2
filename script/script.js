cb88b970aa2b6de2aeb6225d94f5c876c93ca650
Asad425 <asadshanto310@gmail.com>
2021-02-03 00:38:04 +0600

    third

--- script/script.js
+++ script/script.js
@@ -1,10 +1,17 @@
+function plusMinus(whichOne){
+    count=document.getElementById('countValueCover');
+    const countNumber=parseInt(count.value);
+    let caseNewCount
+    if(whichOne=='+'){
+       newCaseCount=countNumber+1;
+       count.value = newCaseCount;
+    }
+    else if(whichOne=='-' && countNumber>0){
+       newCaseCount=countNumber-1;
+       count.value = newCaseCount;
+       
 
-const PlusBtn = document.getElementById('countPlus');
-PlusBtn.addEventListener('click', function () {
-
-
-    const countCover = parseInt(document.getElementById('countValueCover').value);
-    document.getElementById('countValueCover').value = countCover++;
-
-
-});
+    }
+       const caseTotal=newCaseCount*59;
+       document.getElementById('case-total').innerText='$'+caseTotal;
+ }
